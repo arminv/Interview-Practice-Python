a275ff150905b40bc0c2ff2a346e9f4c9c67a773
arminv <arminv@users.noreply.github.com>
2018-09-03 09:25:14 -0400

    initial commit

---
+++ RPS_Game.py
@@ -0,0 +1,61 @@
+import random
+
+def my_Game(a_Choice):
+  '''
+  Takes a choice of 'R' (Rock), 'P' (Paper) or 'S' (Scissors) as 
+  argument and returns the result of playing a round against the
+  computer (random choice).
+  '''
+  
+  choices = ['R', 'P', 'S']
+  win = '--> You WIN! :)'
+  loss = '--> You LOSE! :('
+  draw = '--> It\'s a DRAW!'
+  
+  # Error handling/validation:
+  try:
+    choice = a_Choice[0].upper()
+  except IndexError:
+    print('----> No choice (argument) was given! Please try again! <----')
+    return
+    
+  if choice not in choices:
+    print('----> Your choice is not a valid option! <----')
+    return
+
+  # Computer chooses (pseudo)randomly:
+  computer = random.choice(choices)
+
+  print('  Your choice is: {}'.format(choice))
+  print('  Computer\'s choice is: {}'.format(computer))
+    
+  # Concluding the result of the game: 
+  if choice == computer:
+    print(draw)
+    return
+     
+  else:
+    if choice == 'R':
+      if computer == 'S':
+        print(win)
+      else:
+        print(loss)
+      
+    if choice == 'P':
+      if computer == 'R':
+        print(win)
+      else:
+        print(loss)
+      
+    if choice == 'S':
+      if computer == 'P':
+        print(win)
+      else:
+        print(loss)
+      
+# ----------------------------------------------
+if __name__ == '__main__':
+  your_Choice = raw_input('''
+  What Is Your Choice : (R)ock / (P)aper / (S)cissors ?
+  ''')
+  my_Game(your_Choice)
